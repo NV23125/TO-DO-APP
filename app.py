2e919d7ca4da68087e82cd71fe10a88e5d93df58
Developer <dev@example.com>
2026-02-19 08:35:09 +0000

    WORKING APP

--- app.py
+++ app.py
@@ -155,4 +155,5 @@ def delete_todo(todo_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.getenv('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
